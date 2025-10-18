2ff6cb0fa6735663cf30f7e52faf30be39a67b84
danuntu <danielhulsey@gmail.com>
2025-10-17 21:16:22 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE="redcap-v1";const ASSETS=["./","./index.html","./styles/app.css","./scripts/app.js","./assets/icons/icon-192.png","./assets/icons/icon-512.png","./assets/icons/apple-touch-icon.png","./manifest.json"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))) });
+self.addEventListener("fetch",e=>{const u=new URL(e.request.url); if(u.origin===location.origin){e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))}});
